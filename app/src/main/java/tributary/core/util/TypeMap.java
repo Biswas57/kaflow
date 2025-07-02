ba54f36fcdb623bb6758dd796f48f724e24ee879
Biswas57 <biswas.simkhada@student.unsw.edu.au>
2025-07-02 11:59:38 +1000

    fixing up typeMap utility class in prep for protobuf type conversion

--- app/src/main/java/tributary/core/util/TypeMap.java
+++ app/src/main/java/tributary/core/util/TypeMap.java
@@ -2,32 +2,55 @@ package tributary.core.util;
 
 import java.util.Map;
 import static java.util.Map.entry;
+import com.google.protobuf.*;
 
 public final class TypeMap {
     public static final Map<String, Class<?>> aliasToClass = Map.ofEntries(
             entry("boolean", Boolean.class),
             entry("byte", Byte.class),
             entry("bytes", byte[].class),
-            entry("binary", byte[].class),
-            entry("hex", byte[].class), // optional
-            entry("short", Short.class),
             entry("integer", Integer.class),
+            entry("int", Integer.class),
             entry("long", Long.class),
             entry("float", Float.class),
             entry("double", Double.class),
             entry("string", String.class),
             entry("text", String.class),
-            entry("json", com.fasterxml.jackson.databind.JsonNode.class),
-            entry("xml", org.w3c.dom.Document.class),
-            entry("date", java.time.LocalDate.class),
-            entry("datetime", java.time.LocalDateTime.class),
-            entry("protobuf", com.google.protobuf.ByteString.class),
-            entry("any", com.google.protobuf.Any.class));
+            entry("any", Any.class));
 
     public static Class<?> resolve(String alias) {
         Class<?> typeClass = aliasToClass.get(alias.toLowerCase());
         if (typeClass == null)
             throw new IllegalArgumentException("Unknown type: " + alias);
         return typeClass;
+    }
+
+    // Conversion helpers
+    public static byte[] convert(BytesValue byteValue) {
+        return byteValue.getValue().toByteArray();
+    }
+
+    public static Boolean convert(BoolValue boolValue) {
+        return boolValue.getValue();
+    }
+
+    public static String convert(StringValue stringValue) {
+        return stringValue.getValue();
+    }
+
+    public static Integer convert(Int32Value intValue) {
+        return intValue.getValue();
+    }
+
+    public static Long convert(Int64Value longValue) {
+        return longValue.getValue();
+    }
+
+    public static Float convert(FloatValue floatValue) {
+        return floatValue.getValue();
+    }
+
+    public static Double convert(DoubleValue doubleValue) {
+        return doubleValue.getValue();
     }
 }
